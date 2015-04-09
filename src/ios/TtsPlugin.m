1190f9135dcd3ee3f05a02ba3fe60db8067784ee
Joshua Cave <josh@loveinweb.com>
2015-04-09 16:26:29 +1200

    get speech status

--- src/ios/TtsPlugin.m
+++ src/ios/TtsPlugin.m
@@ -51,15 +51,19 @@ double rate = .2;
 - (void)getSpeechStatus:(CDVInvokedUrlCommand*)command{
     bool isSpeaking = [synth isSpeaking];
     bool isPaused = [synth isSpeaking];
-    NSMutableString *speechStatus = @"stopped";
+    CDVPluginResult* pluginResult = null
+     //NSString *speechStatus = @"stopped";
 
     if (isSpeaking && isPaused){
-        *speechStatus = @"paused";
+        // *speechStatus = @"paused";
+        [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"paused"];
     }else if(isSpeaking){
-        *speechStatus = @"speaking";
+        // *speechStatus = @"speaking";
+        [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"speaking"];
+    }else{
+        [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"stopped"];
     }
 
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:speechStatus];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
