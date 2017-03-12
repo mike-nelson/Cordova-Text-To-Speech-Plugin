0bdf5938552ba62d150e01d730c06cff7c11b11c
Mike Nelson <mike@beweb.co.nz>
2017-03-13 08:08:32 +1300

    added AVAudioSessionModeSpokenAudio

--- src/ios/TtsPlugin.m
+++ src/ios/TtsPlugin.m
@@ -1,16 +1,43 @@
 #import "TtsPlugin.h"
 #import <Cordova/CDV.h>
+#import <AVFoundation/AVFoundation.h>
 
 @implementation TtsPlugin
 
 AVSpeechSynthesizer *synth;
+//NSString *lang = @"en-US";
 NSString *lang = @"en-US";
-double rate = .2;
+AVSpeechSynthesisVoice *globalVoice;
+double rate = 0.2;
+NSString *currentLocale;
 
 - (void)initTTS:(CDVInvokedUrlCommand*)command{
     synth = [[AVSpeechSynthesizer alloc] init];
     synth.delegate = self;
 
+    NSString *locale = [[NSLocale currentLocale] localeIdentifier];
+    NSRange startRange = [locale rangeOfString:@"_"]; // this isn't the underscore you see in the first string
+    currentLocale = [locale stringByReplacingCharactersInRange:NSMakeRange(0, startRange.length+1) withString:[[NSLocale preferredLanguages] objectAtIndex:0]];
+    currentLocale = [currentLocale stringByReplacingOccurrencesOfString:@"_"
+                                         withString:@"-"];
+    //NSLog(@"current locale: %@", currentLocale);
+
+    if (&AVAudioSessionModeSpokenAudio!=nil){
+        AVAudioSession *session = [AVAudioSession sharedInstance];
+        [session setMode:AVAudioSessionModeSpokenAudio error:nil];
+    }
+    
+    for (AVSpeechSynthesisVoice *voice in [AVSpeechSynthesisVoice speechVoices]) {
+        /*NSLog(@"voice: %@", voice.language);*/
+        NSString *language = voice.language;
+        
+        if ([language isEqualToString:currentLocale]){
+            /*NSLog(@"setting voice to locale: %@", currentLocale);*/
+            globalVoice = voice;
+        }
+    }
+    
+    
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"OK"];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
@@ -24,6 +51,7 @@ double rate = .2;
 - (void)setRate:(CDVInvokedUrlCommand*)command{
     @try {
         rate = [[command.arguments objectAtIndex:0] doubleValue];
+        //NSLog(@"setting rate: %f", rate);
         CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"OK"];
         [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
     }
@@ -34,14 +62,64 @@ double rate = .2;
     //@finally { }
 }
 
+- (void)getVoices:(CDVInvokedUrlCommand*)command{
+    /*NSLog(@"setting voice to locale: coolbeans");*/
+    
+    NSMutableArray *stringArray = [[NSMutableArray alloc] init];
+    for (AVSpeechSynthesisVoice *voice in [AVSpeechSynthesisVoice speechVoices]) {
+        NSString *nameAndLocale = [voice.name stringByAppendingString:@"["];
+        nameAndLocale = [nameAndLocale stringByAppendingString:voice.language];
+        nameAndLocale = [nameAndLocale stringByAppendingString:@"]"];
+        [stringArray addObject:nameAndLocale];
+    }
+    
+    [self.commandDelegate sendPluginResult:[CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsArray:stringArray] callbackId:command.callbackId];
+}
+
+- (void)setVoice:(CDVInvokedUrlCommand*)command{
+    NSString* text = [command.arguments objectAtIndex:0];
+    
+    for (AVSpeechSynthesisVoice *voice in [AVSpeechSynthesisVoice speechVoices]) {
+        /*NSLog(@"voice: %@", voice.language);
+        NSLog(@"name: %@", voice.name);*/
+        //NSString *language = voice.language;
+        NSString *name = voice.name;
+        
+        if ([name isEqualToString:text]){
+            /*NSLog(@"setting voice to locale: %@", currentLocale);*/
+            globalVoice = voice;
+        }
+        
+//        
+//        if ([language isEqualToString:text]){
+//            NSLog(@"setting voice to locale: %@", currentLocale);
+//            globalVoice = voice;
+//        }
+        
+    }
+    
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:globalVoice.language];
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}
+
+
 - (void)speak:(CDVInvokedUrlCommand*)command{
     NSString* text = [command.arguments objectAtIndex:0];
+    //AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:@"by not setting it it should use the default cool cool"];
     AVSpeechUtterance *utterance = [AVSpeechUtterance speechUtteranceWithString:text];
-    utterance.voice = [AVSpeechSynthesisVoice voiceWithLanguage:lang];
-    utterance.rate = rate;
+//    utterance.voice = [AVSpeechSynthesisVoice voiceWithLanguage:lang];
+    utterance.voice = globalVoice;
+    //utterance.voice = [AVSpeechSynthesisVoice currentLanguageCode]; JC didnt work... 20150609 by not setting it it should use the default locale
+    
+    //NSLog(@"max: %f", AVSpeechUtteranceMaximumSpeechRate);
+    //NSLog(@"min: %f", AVSpeechUtteranceMinimumSpeechRate);
+    //NSLog(@"default: %f", AVSpeechUtteranceDefaultSpeechRate);
+
+    utterance.rate = rate*AVSpeechUtteranceDefaultSpeechRate/0.2;
+    //NSLog(@"current: %f", utterance.rate);
     [synth speakUtterance:utterance];
 
-    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:text];
+    CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"OK"];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
@@ -55,8 +133,7 @@ double rate = .2;
     bool isSpeaking = [synth isSpeaking];
     bool isPaused = [synth isPaused];
     CDVPluginResult* pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"stopped"];
-     //NSString *speechStatus = @"stopped";
-
+    
     if (isSpeaking && isPaused){
         // *speechStatus = @"paused";
         pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"paused"];
@@ -88,27 +165,27 @@ double rate = .2;
 }
 
 - (void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didStartSpeechUtterance:(AVSpeechUtterance *)utterance{
-    NSLog(@"Started Speaking");
+    NSLog(@"Started Speaking %@", utterance);
     [self.commandDelegate evalJs:@"ttsPlugin.callbacks.startedSpeaking()"];
 }
 - (void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didFinishSpeechUtterance:(AVSpeechUtterance *)utterance{
-    NSLog(@"Stopped Speaking");
+    NSLog(@"Stopped Speaking %@", utterance);
     [self.commandDelegate evalJs:@"ttsPlugin.callbacks.finishedSpeaking()"];
 }
 - (void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didPauseSpeechUtterance:(AVSpeechUtterance *)utterance{
-    NSLog(@"Paused Speaking");
+    NSLog(@"Paused Speaking %@", utterance);
     [self.commandDelegate evalJs:@"ttsPlugin.callbacks.pausedSpeaking()"];
 }
 - (void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didContinueSpeechUtterance:(AVSpeechUtterance *)utterance{
-    NSLog(@"Continued Speaking");
+    NSLog(@"Continued Speaking %@ ", utterance);
     [self.commandDelegate evalJs:@"ttsPlugin.callbacks.continuedSpeaking()"];
 }
 - (void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer didCancelSpeechUtterance:(AVSpeechUtterance *)utterance{
-    NSLog(@"Cancelled Speaking");
+    NSLog(@"Cancelled Speaking %@", utterance);
     [self.commandDelegate evalJs:@"ttsPlugin.callbacks.cancelledSpeaking()"];
 }
 - (void)speechSynthesizer:(AVSpeechSynthesizer *)synthesizer willSpeakRangeOfSpeechString:(NSRange)characterRange utterance:(AVSpeechUtterance *)utterance{
-    NSLog(@"willSpeakRangeOfSpeechString: %@", NSStringFromRange(characterRange));
+    //NSLog(@"willSpeakRangeOfSpeechString: %@", NSStringFromRange(characterRange));
     NSString* jsString = [[NSString alloc] initWithFormat:@"ttsPlugin.callbacks.currentRangeOfSpeech(\"%@\")",NSStringFromRange(characterRange)];
     [self.commandDelegate evalJs:jsString];
 }
