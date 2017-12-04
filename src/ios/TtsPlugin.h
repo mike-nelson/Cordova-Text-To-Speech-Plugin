ae5900a0bae438e06bb72f4bfd1df4d866c49f39
Mike Nelson <mike@beweb.co.nz>
2017-12-05 00:18:51 +1300

    added getVoices

--- src/ios/TtsPlugin.h
+++ src/ios/TtsPlugin.h
@@ -1,11 +1,13 @@
 #import <Cordova/CDV.h>
 #import <AVFoundation/AVFoundation.h>
 
-@interface TtsPlugin : CDVPlugin
+@interface TtsPlugin : CDVPlugin <AVSpeechSynthesizerDelegate>
 
 - (void)initTTS:(CDVInvokedUrlCommand*)command;
 - (void)setRate:(CDVInvokedUrlCommand*)command;
 - (void)setLanguage:(CDVInvokedUrlCommand*)command;
+- (void)setVoice:(CDVInvokedUrlCommand*)command;
+- (void)getVoices:(CDVInvokedUrlCommand*)command;
 - (void)speak:(CDVInvokedUrlCommand*)command;
 - (void)getSpeechStatus:(CDVInvokedUrlCommand*)command;
 - (void)stop:(CDVInvokedUrlCommand*)command;
